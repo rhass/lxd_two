92dfb15dc0143eaf1a219040144b7f0e10313b32
Ryan Hass <rhass@users.noreply.github.com>
2018-01-02 14:00:47 -0800

    Tune sysctl parameters based on lxd setup guide.
    Tuned per the recommendations in:
    https://github.com/lxc/lxd/blob/master/doc/production-setup.md
    
    Signed-off-by: Ryan Hass <rhass@users.noreply.github.com>

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,7 @@ maintainer_email 'ryan@invalidchecksum.net'
 license 'Apache-2.0'
 description 'Installs/Configures lxd_two'
 long_description 'Installs/Configures lxd_two'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 13.0' if respond_to?(:chef_version)
 issues_url 'https://github.com/rhass>/lxd_two/issues'
 source_url 'https://github.com/rhass>/lxd_two'
-
-depends 'apt'
--- recipes/configure.rb
+++ recipes/configure.rb
@@ -17,6 +17,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# LXD Production Server Tuning
+# https://github.com/lxc/lxd/blob/master/doc/production-setup.md
+file '/etc/security/limits.d/60-lxd.conf' do
+  owner 'root'
+  group 'root'
+  mode 0644
+  content <<EOF
+# ***
+# * THIS FILE WAS CREATED AND IS MANAGED BY CHEF
+# * Cookbook:: #{cookbook_name}
+# * Recipe:: #{recipe_name}
+# ***
+#<domain>      <type>  <item>         <value>
+*              soft    nofile         1048576
+*              hard    nofile         1048576
+root           soft    nofile         1048576
+root           hard    nofile         1048576
+*              soft    memlock        unlimited
+*              hard    memlock        unlimited
+EOF
+end
+
+file '/etc/sysctl.d/60-lxd.conf' do
+  owner 'root'
+  group 'root'
+  mode 0644
+  content <<EOF
+# ***
+# * THIS FILE WAS CREATED AND IS MANAGED BY CHEF
+# * Cookbook:: #{cookbook_name}
+# * Recipe:: #{recipe_name}
+# ***
+fs.inotify.max_queued_events=1048576
+fs.inotify.max_user_instances=1048576
+fs.inotify.max_user_watches=1048576
+vm.max_map_count=262144
+kernel.dmesg_restrict=1
+net.core.netdev_max_backlog=#{Mixlib::ShellOut.new('sysctl net.ipv4.tcp_mem').run_command.stdout.split[2]}
+EOF
+end
+
+# Reload sysctl files when creating or modifying the conf.
+execute 'apply_sysctl_settings' do
+  command 'sysctl --system'
+  action :nothing
+  subscribes :run, 'file[/etc/sysctl.d/60-lxd.conf]'
+end
+
 execute 'lxd_init' do
   command "echo \"#{node['lxd']['preseed'].to_hash.to_yaml}\" | lxd init --preseed"
   sensitive true
